884d4da3d1cffb190217b015c6d31eac705612c4
artem_nanavov <artemnanavov@gmail.com>
2021-01-13 19:07:30 +0300

    Updates

---
+++ precache-manifest.05650702ef158fa7da33bc25a47d5f90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa55ff210161850e97a16d5dcd2b2ff0",
+    "url": "/toUp/index.html"
+  },
+  {
+    "revision": "aa4f6f99081b3f08114b",
+    "url": "/toUp/static/css/2.2d12201f.chunk.css"
+  },
+  {
+    "revision": "d62fce7cb09c995795b0",
+    "url": "/toUp/static/css/main.a2a28887.chunk.css"
+  },
+  {
+    "revision": "aa4f6f99081b3f08114b",
+    "url": "/toUp/static/js/2.94f0a39b.chunk.js"
+  },
+  {
+    "revision": "f666cac220317b2215e442d16825ea61",
+    "url": "/toUp/static/js/2.94f0a39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62fce7cb09c995795b0",
+    "url": "/toUp/static/js/main.6c51655f.chunk.js"
+  },
+  {
+    "revision": "795646bd2ca570903b12",
+    "url": "/toUp/static/js/runtime-main.6aedf1ab.js"
+  },
+  {
+    "revision": "ce5e9ea12ed5ba96d4bc041a64cb0b0d",
+    "url": "/toUp/static/media/Mask Group.ce5e9ea1.jpg"
+  }
+]);
